e19d698eba7126a7d992f6ba269a7ad2593a5956
Nadya <meng.meiyingg@gmail.com>
2021-12-12 12:31:42 +0300

    added dialogs page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,25 @@ import styles from "./Dialogs.module.css";
 
 const Dialogs = () => {
     return (
-        <div>Content</div>
+        <div>
+            <h1 className={styles.heading}>Dialogs</h1>
+            <div className={styles.wrapper}>
+                <ul className={styles.names_list}>
+                    <li className={`${styles.names_item} ${styles.active}`}><a href="">Andrew</a></li>
+                    <li className={styles.names_item}><a href="">Sasha</a></li>
+                </ul>
+                <div className={styles.chat}>
+                    <div className={styles.message}>
+                        <img className={styles.avatar} src="https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png" alt="" />
+                        <p className={styles.text}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
+                            aliquid impedit assumenda corrupti commodi temporibus, repellendus sunt a sapiente odit,
+                            nemo laboriosam necessitatibus amet. Debitis provident obcaecati eius aut natus!</p>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
     );
 }
 
