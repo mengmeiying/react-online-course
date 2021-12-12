7f109853a6b27694ada71def6958c6957dc5bbc4
Nadya <meng.meiyingg@gmail.com>
2021-12-12 11:59:29 +0300

    added navlinks

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
   return (
     <nav className={styles.nav}>
       <ul className={styles.list}>
-        <li><NavLink className={styles.link} to="/dialogs">Messages</NavLink></li>
-        <li><NavLink className={styles.link} to="/news">News</NavLink></li>
-        <li><NavLink className={styles.link} to="/profile">Profile</NavLink></li>
-        <li><NavLink className={styles.link} to="/music">Music</NavLink></li>
-        <li><NavLink className={styles.link} to="/settings">Settings</NavLink></li>
+        <li><NavLink className={navData => navData.isActive ? (styles.active + " " + styles.link) : styles.link} to="/dialogs">Messages</NavLink></li>
+        <li><NavLink className={navData => navData.isActive ? (styles.active + " " + styles.link) : styles.link} to="/news">News</NavLink></li>
+        <li><NavLink className={navData => navData.isActive ? (styles.active + " " + styles.link) : styles.link} to="/profile">Profile</NavLink></li>
+        <li><NavLink className={navData => navData.isActive ? (styles.active + " " + styles.link) : styles.link} to="/music">Music</NavLink></li>
+        <li><NavLink className={navData => navData.isActive ? (styles.active + " " + styles.link) : styles.link} to="/settings">Settings</NavLink></li>
       </ul>
     </nav>
   )
