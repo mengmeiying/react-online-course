3925887f1f14e17afc3d51dcebd47bf2d5a0f625
Nadya <meng.meiyingg@gmail.com>
2021-12-14 14:02:41 +0300

    added data layer to posts and dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,22 +4,62 @@ import Message from "./Message/Message";
 import NamesItem from "./NamesItem/NamesItem";
 
 const Dialogs = () => {
+    let dialogsData = [
+        {
+            id: 1, 
+            name: "Andrew"
+        },
+        {
+            id: 2, 
+            name: "Maria"
+        },
+        {
+            id: 3, 
+            name: "Alex"
+        },
+        {
+            id: 4, 
+            name: "Nadya"
+        },
+    ];
+
+    let messagesData = [
+        {
+            id: 1,
+            src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
+            text: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
+            aliquid impedit assumenda corrupti commodi temporibus, repellendus sunt a sapiente odit,
+            nemo laboriosam necessitatibus amet. Debitis provident obcaecati eius aut natus!`
+        },
+        {
+            id: 2,
+            src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
+            text: `Hello world`
+        },
+        {
+            id: 3,
+            src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
+            text: `ok google how to write`
+        }
+    ]
     return (
         <div>
             <h1 className={styles.heading}>Dialogs</h1>
             <div className={styles.wrapper}>
                 <ul className={styles.names_list}>
-                    <NamesItem number='1' name="Andrew"/>
-                    <NamesItem number='2' name="Sasha"/>
-                    <NamesItem number='3' name="Nadya"/>
+                    <NamesItem number={dialogsData[0].id} name={dialogsData[0].name}/>
+                    <NamesItem number={dialogsData[1].id} name={dialogsData[1].name}/>
+                    <NamesItem number={dialogsData[2].id} name={dialogsData[2].name}/>
+                    <NamesItem number={dialogsData[3].id} name={dialogsData[3].name}/>
                 </ul>
                 <ul className={styles.chat_list}>
-                    <Message src="https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png"
-                            text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
-                            aliquid impedit assumenda corrupti commodi temporibus, repellendus sunt a sapiente odit,
-                            nemo laboriosam necessitatibus amet. Debitis provident obcaecati eius aut natus!"/>
-                            <Message src="https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png"
-                            text="Hello world!"/>
+                    <Message src={messagesData[0].src}
+                            text={messagesData[0].text}/>
+                    <Message src={messagesData[1].src}
+                            text={messagesData[1].text}/>
+                    <Message src={messagesData[2].src}
+                            text={messagesData[2].text}/>
+                            
                 </ul>
             </div>
 
--- src/components/Profile/MyPosts/Posts.jsx
+++ src/components/Profile/MyPosts/Posts.jsx
@@ -3,17 +3,49 @@ import styles from "./Posts.module.css";
 import Post from "./Post/Post"
 
 const Posts = () => {
-    return (  
-      <div className={styles.posts}>
-          <h2>My posts:</h2>
-          <Form />
-          <ul className={styles.posts_list}>
-            <li className={styles.post}><Post text="post 1" likes="1"/></li>
-            <li className={styles.post}><Post text="post 2" likes="15"/></li>
-            <li className={styles.post}><Post text="post 3" likes="21"/></li>
-          </ul>
-        </div>
-    )
+  let postsData = [
+    {
+      id: 1,
+      text: `post 1`,
+      likes: 1
+    },
+    {
+      id: 2,
+      text: `post 2`,
+      likes: 14
+    },
+    {
+      id: 3,
+      text: `post 3`,
+      likes: 32
+    },
+    {
+      id: 4,
+      text: `post 4`,
+      likes: 8
+    },
+  ]
+
+  return (
+    <div className={styles.posts}>
+      <h2>My posts:</h2>
+      <Form />
+      <ul className={styles.posts_list}>
+        <li className={styles.post}>
+          <Post text={postsData[0].text} likes={postsData[0].likes} />
+        </li>
+        <li className={styles.post}>
+          <Post text={postsData[1].text} likes={postsData[1].likes} />
+        </li>
+        <li className={styles.post}>
+          <Post text={postsData[2].text} likes={postsData[2].likes} />
+        </li>
+        <li className={styles.post}>
+          <Post text={postsData[3].text} likes={postsData[3].likes} />
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Posts;
