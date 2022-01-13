cda26ce8b7421a984f64a6783ce379ede3f1e74b
Nadya <meng.meiyingg@gmail.com>
2022-01-13 20:01:46 +0300

    added onclick in dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,17 @@
 import styles from "./Dialogs.module.css";
 import Message from "./Message/Message";
 import NamesItem from "./NamesItem/NamesItem";
+import React from "react";
 
 const Dialogs = (props) => {
     let dialogsElements = props.state.dialogsData.map( dialog => <NamesItem number={dialog.id} name={dialog.name}/>);
 
     let messagesElements = props.state.messagesData.map(message => <Message src={message.src} text={message.text}/>);
+
+    let newMessageElement = React.createRef();
+    let onButtonClick = () => {
+        alert(newMessageElement.current.value)
+    }
 
     return (
         <div>
@@ -14,9 +20,14 @@ const Dialogs = (props) => {
                 <ul className={styles.names_list}>
                     {dialogsElements}
                 </ul>
+                <div>
                 <ul className={styles.chat_list}>
                     {messagesElements} 
                 </ul>
+                <textarea ref={newMessageElement} cols="30" rows="10"></textarea>
+                <button onClick={onButtonClick}>Send</button>
+                </div>
+                
             </div>
 
         </div>
