ffe990e38978824b2c9b403bca9ec5595a6c7fd2
Nadya <meng.meiyingg@gmail.com>
2022-01-22 14:58:50 +0300

    added oop store object instead of state and functions

--- src/index.js
+++ src/index.js
@@ -3,23 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import state, { subscribe } from './redux/state';
-import {addMessage, addPost, updateNewPostText, updateNewMessageText} from './redux/state';
-
+import store from './redux/state';
 
 let rerenderEntireTree = () => {
-    ReactDOM.render(
-      <React.StrictMode>
-        <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} addMessage={addMessage} updateNewMessageText={updateNewMessageText}/>
-      </React.StrictMode>,
-      document.getElementById('root')
-    );
-  }
+  ReactDOM.render(
+    <React.StrictMode>
+      <App state={store.getState()}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
+        addMessage={store.addMessage.bind(store)}
+        updateNewMessageText={store.updateNewMessageText.bind(store)} />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
 
 
 rerenderEntireTree();
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,131 +1,129 @@
-let rerenderEntireTree = () => {
-    
-}
+let store = {
+    _state: {
+        profilePage: {
+            postsData: [
+                {
+                    id: 1,
+                    text: `post 1`,
+                    likes: 1
+                },
+                {
+                    id: 2,
+                    text: `post 2`,
+                    likes: 14
+                },
+                {
+                    id: 3,
+                    text: `post 3`,
+                    likes: 32
+                },
+                {
+                    id: 4,
+                    text: `post 4`,
+                    likes: 8
+                },
+            ],
+            newPostText: 'enter text'
+        },
 
-let state = {
-    profilePage: {
-        postsData: [
-            {
-                id: 1,
-                text: `post 1`,
-                likes: 1
-            },
-            {
-                id: 2,
-                text: `post 2`,
-                likes: 14
-            },
-            {
-                id: 3,
-                text: `post 3`,
-                likes: 32
-            },
-            {
-                id: 4,
-                text: `post 4`,
-                likes: 8
-            },
-        ],
-        newPostText: 'enter text'
+        dialogsPage: {
+            messagesData: [
+                {
+                    id: 1,
+                    src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
+                    text: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
+                        aliquid impedit assumenda corrupti commodi temporibus, repellendus sunt a sapiente odit,
+                        nemo laboriosam necessitatibus amet. Debitis provident obcaecati eius aut natus!`
+                },
+                {
+                    id: 2,
+                    src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
+                    text: `Hello world`
+                },
+                {
+                    id: 3,
+                    src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
+                    text: `ok google how to write`
+                }
+            ],
+            dialogsData: [
+                {
+                    id: 1,
+                    name: "Andrew"
+                },
+                {
+                    id: 2,
+                    name: "Maria"
+                },
+                {
+                    id: 3,
+                    name: "Alex"
+                },
+                {
+                    id: 4,
+                    name: "Nadya"
+                },
+            ],
+            newMessageText: 'enter text'
+        },
+
+        navPage: {
+            friendsData: [
+                {
+                    id: 1,
+                    src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
+                    name: `Masha`
+                },
+                {
+                    id: 2,
+                    src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
+                    name: `Sasha`
+                },
+                {
+                    id: 3,
+                    src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
+                    name: `Dasha`
+                }
+            ]
+        }
     },
-
-    dialogsPage: {
-        messagesData: [
-            {
-                id: 1,
-                src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
-                text: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi
-                    aliquid impedit assumenda corrupti commodi temporibus, repellendus sunt a sapiente odit,
-                    nemo laboriosam necessitatibus amet. Debitis provident obcaecati eius aut natus!`
-            },
-            {
-                id: 2,
-                src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
-                text: `Hello world`
-            },
-            {
-                id: 3,
-                src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
-                text: `ok google how to write`
-            }
-        ],
-        dialogsData: [
-            {
-                id: 1,
-                name: "Andrew"
-            },
-            {
-                id: 2,
-                name: "Maria"
-            },
-            {
-                id: 3,
-                name: "Alex"
-            },
-            {
-                id: 4,
-                name: "Nadya"
-            },
-        ],
-        newMessageText: 'enter text'
+    getState() {
+        return this._state;
     },
+    _callSubscriber() {
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber();
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            text: this._state.profilePage.newPostText,
+            likes: 0
+        };
+        this._state.profilePage.postsData.push(newPost);
 
-    navPage: {
-        friendsData: [
-            {
-                id: 1,
-                src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
-                name: `Masha`
-            },
-            {
-                id: 2,
-                src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
-                name: `Sasha`
-            },
-            {
-                id: 3,
-                src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
-                name: `Dasha`
-            }
-        ]
-    }
-};
-
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree();
+        this._callSubscriber();
+        this.updateNewPostText('');
+    },
+    updateNewMessageText(newText) {
+        this._state.dialogsPage.newMessageText = newText;
+        this._callSubscriber();
+    },
+    addMessage() {
+        let newMessage = {
+            id: 4,
+            src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
+            text: this._state.dialogsPage.newMessageText
+        }
+        this._state.dialogsPage.messagesData.push(newMessage);
+        this._callSubscriber();
+        this.updateNewMessageText('');
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
+    },
 }
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        text: state.profilePage.newPostText,
-        likes: 0
-    };
-    state.profilePage.postsData.push(newPost);
-
-    rerenderEntireTree();
-    updateNewPostText('');
-};
-
-export const updateNewMessageText = (newText) => {
-    state.dialogsPage.newMessageText = newText;
-    rerenderEntireTree();
-}
-
-export const addMessage = () => {
-    let newMessage = {
-        id: 4,
-        src: `https://www.seekpng.com/png/full/115-1150622_avatar-demo2x-man-avatar-icon-png.png`,
-        text: state.dialogsPage.newMessageText
-    }
-    state.dialogsPage.messagesData.push(newMessage);
-    rerenderEntireTree();
-    updateNewMessageText('');
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-export default state;
+export default store;
