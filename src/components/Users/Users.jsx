2ffa8698fad1488ec5af878797e8d0e7249b4729
Nadya <meng.meiyingg@gmail.com>
2022-02-16 12:12:21 +0300

    changed users from function to class component

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,47 +1,60 @@
+import React from "react";
 import axios from "axios";
 import styles from "./Users.module.css"
 import userImage from "./../../assets/images/userImage.png";
 
-const Users = (props) => {
-    let getUsers = () => {
-        if (props.usersData.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setUsers(response.data.items);
-            });
-    
-        }
+class Users extends React.Component {
+    constructor(props) {
+        super(props)
+        if (this.props.usersData.length === 0) {
+                    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                        this.props.setUsers(response.data.items);
+                    });
+        
+                }
     }
     
-    return (
-        <div>
-            {
-                props.usersData.map(u =>
-                    <div className={styles.user} key={u.id}>
-                        <div className={styles.leftBlock}>
-                            <img className={styles.avatar} src={u.photos.small !== null ? u.photos.small : userImage} alt="" />
-                            {
-                                u.followed
-                                    ? <button className={styles.follow} onClick={() => { props.unfollow(u.id) }}>Unfollow</button>
-                                    : <button className={styles.follow} onClick={() => { props.follow(u.id) }}>Follow</button>
-                            }
-                        </div>
-                        <div className={styles.userInfo}>
-                            <div className={styles.top}>
-                                <h3 className={styles.userName}>{u.name}</h3>
+    // getUsers = () => {
+    //     if (this.props.usersData.length === 0) {
+    //         axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+    //             this.props.setUsers(response.data.items);
+    //         });
+
+    //     }
+    // }
+    render() {
+        return (
+            <div>
+                {
+                    this.props.usersData.map(u =>
+                        <div className={styles.user} key={u.id}>
+                            <div className={styles.leftBlock}>
+                                <img className={styles.avatar} src={u.photos.small !== null ? u.photos.small : userImage} alt="" />
+                                {
+                                    u.followed
+                                        ? <button className={styles.follow} onClick={() => { this.props.unfollow(u.id) }}>Unfollow</button>
+                                        : <button className={styles.follow} onClick={() => { this.props.follow(u.id) }}>Follow</button>
+                                }
+                            </div>
+                            <div className={styles.userInfo}>
+                                <div className={styles.top}>
+                                    <h3 className={styles.userName}>{u.name}</h3>
+                                    <div>
+                                        <p>moscow,</p>
+                                        <p>russia</p>
+                                    </div>
+                                </div>
                                 <div>
-                                    <p>moscow,</p>
-                                    <p>russia</p>
+                                    <p className={styles.status}>{u.status}</p>
                                 </div>
                             </div>
-                            <div>
-                                <p className={styles.status}>{u.status}</p>
-                            </div>
-                        </div>
-                    </div>)
-            }
-            <button onClick={getUsers}>Get users</button>
-        </div>
-    )
+                        </div>)
+                }
+                {/* <button onClick={this.getUsers}>Get users</button> */}
+            </div>
+        )
+    }
 }
 
 export default Users;
+
